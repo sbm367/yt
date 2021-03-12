245a5c2d4dbd9a93246418043548226309ff3eb1
sbm367 <sbm367@protonmail.com>
2021-03-11 22:13:05 -0500

    got dynamic file naming/saving to work, need to add subdirectory and save as json, not just sbo/pickle

--- yt.py
+++ yt.py
@@ -4,9 +4,9 @@ from apiclient.discovery import build
 import requests
 import json
 import pickle
+import datetime
 
 
-'''
 print(api_key)
 
 API_SERVICE_NAME = 'youtube'
@@ -16,8 +16,8 @@ API_VERSION = 'v3'
 
 # accept & parse input
 
-yt_url = 'https://www.youtube.com/channel/UCSJ4gkVC6NrvII8umztf0Ow'
-#yt_url = input('enter url of youtube chanel')
+#yt_url = 'https://www.youtube.com/channel/UCSJ4gkVC6NrvII8umztf0Ow'
+yt_url = input('enter url of youtube chanel : ')
 print(yt_url)
 splt = yt_url.split('/')
 print(splt)
@@ -38,33 +38,53 @@ chan_req = youtube.channels().list(id=chan_id,  part='snippet')
 chan = chan_req.execute()
 
 # Saved the returned object
-#f = open("api_return_ex_1", "w")
+
+
+## dynamicly chnage the name var to chan name
+## and current date so the sved objects 
+## are easier to read / process
+chan_name = chan['items'][0]['snippet']['title']
+time_stamp = datetime.datetime.now()
+fln = [chan_name,str(time_stamp)]
+filename = ','.join(fln)
+
+# Need to add a subdirectory to store these in
+
+# File or pickle?, ultimatly sql db
+#getting error cause cant just write dic
+#need to save as json
+#f = open(filename, "w")
 #f.write(chan)
 #f.close()
-outfile = open('api_return_ex_1','wb')
+
+outfile = open(filename,'wb')
 pickle.dump(chan, outfile)
 outfile.close()
-'''
+
+
+
 #open and read the file after the appending:
 #f = open("api_return_ex_1", "r")
 #chan_read = f.read()
 #print(f.read())
-print('this is just coming from a file')
-infile = open('api_return_ex_1','rb')
+
+print('this is just coming from a file \n')
+infile = open(filename,'rb')
 new_dict = pickle.load(infile)
 infile.close()
 
 # Process the returned data 
+print(new_dict)
+print('============================================')
 for key in new_dict.keys():
 	print(key,':',new_dict[key])
 
 #print(chan)
-#%%
-for item in chan:
-	print(item)
- #%#%
+#for item in chan:
+#	print(item)
 
-vid_pl_req = youtube.playlistItems().list(playlistId='UUsoiSpBvkr4Y-78Pj3recUw', maxResults=50,  part='snippet')
-vid_pl= vid_pl_req.execute()
-for item in vid_pl['items']:
- 	print(item['snippet']['resourceId']['videoId'])
+
+#vid_pl_req = youtube.playlistItems().list(playlistId='UUsoiSpBvkr4Y-78Pj3recUw', maxResults=50,  part='snippet')
+#vid_pl= vid_pl_req.execute()
+#for item in vid_pl['items']:
+# 	print(item['snippet']['resourceId']['videoId'])
