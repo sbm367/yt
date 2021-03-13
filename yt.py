deafb92c44892a5f249641afce7e06fcf7e8dd9c
sbm367 <sbm367@protonmail.com>
2021-03-13 00:03:23 -0500

    moved save routine to seperate fcn, encountered edge case with chan name urls

--- yt.py
+++ yt.py
@@ -11,20 +11,38 @@ print(api_key)
 
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
+youtube = build(API_SERVICE_NAME,API_VERSION,developerKey=api_key)
+
 
 #%#%
 
 # accept & parse input
 
-#yt_url = 'https://www.youtube.com/channel/UCSJ4gkVC6NrvII8umztf0Ow'
+# yt_url = 'https://www.youtube.com/channel/UCSJ4gkVC6NrvII8umztf0Ow'
+# Note, need to test edge cases like this https://www.youtube.com/c/TheCodingTrain
+# and this https://www.youtube.com/user/derekbanas
+# and this https://www.youtube.com/c/khanacademy
+# and this https://www.youtube.com/user/arduinoversusevil
+# https://www.youtube.com/user/numberphile
+# ok wat the heck, how common is this? 
 
-def get_yt_chan( yt_url ):
+def save_obj(f_name, f):
+	time_stamp = datetime.datetime.now()
+	fln = [f_name,str(time_stamp)]
+	filename = ','.join(fln)
+
+	outfile = open(filename,'wb')
+	pickle.dump(f, outfile)
+	outfile.close()
+
+	return True
+
+def get_yt_chan( yt, yt_url ):
 	splt = yt_url.split('/')
 	chan_id = splt[-1]
 	# build the base object
-	youtube = build(API_SERVICE_NAME,API_VERSION,developerKey=api_key)
 	# build the request object
-	chan_req = youtube.channels().list(id=chan_id,  part='snippet')
+	chan_req = yt.channels().list(id=chan_id,  part='snippet')
 	# Execute
 	chan = chan_req.execute()
 
@@ -33,16 +51,19 @@ def get_yt_chan( yt_url ):
 	## dynamicly chnage the name var to chan name
 	## and current date so the sved objects 
 	## are easier to read / process
-	chan_name = chan['items'][0]['snippet']['title']
-	time_stamp = datetime.datetime.now()
-	fln = [chan_name,str(time_stamp)]
-	filename = ','.join(fln)
 
-	outfile = open(filename,'wb')
-	pickle.dump(chan, outfile)
-	outfile.close()
+	#this seems to specfic to chanel, 
+	chan_name = chan['items'][0]['snippet']['title']
+
+	save_obj(chan_name, chan)
 
 	return chan
+
+def get_yt_chan_vids(chan):
+	return 'code this later'
+
+def get_yt_vid_coms(vid):
+	return 'code this later'
 
 # Need to add a subdirectory to store these in
 
@@ -65,7 +86,7 @@ def get_yt_chan( yt_url ):
 
 
 url = input('enter url of youtube chanel : ')
-new_dic = get_yt_chan(url)
+new_dic = get_yt_chan(youtube, url)
 
 # Process the returned data 
 print(new_dic)
